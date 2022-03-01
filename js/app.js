8dab9d3d217243e6a6f608b089bad1bfb06795f0
Kasia Zaleska <kzaleska416@gmail.com>
2022-03-01 19:08:28 +0100

    change map's location

--- js/app.js
+++ js/app.js
@@ -5,21 +5,52 @@ const locationInfo = document.getElementById("location");
 const timezoneInfo = document.getElementById("timezone");
 const ispInfo = document.getElementById("isp");
 
+let MAP, MARKER;
+
+let url =
+  "https://geo.ipify.org/api/v2/country,city?apiKey=at_t9IcNihh5h1gVRAoIiriG3yL1j45g&";
+
+const initializeMap = () => {
+  MAP = L.map("map").setView([51.505, -0.09], 13);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(MAP);
+
+  const icon = L.icon({
+    iconUrl: "./images/icon-location.svg",
+    iconSize: [24, 34],
+  });
+
+  MARKER = L.marker([51.5, -0.09], { icon: icon }).addTo(MAP);
+};
 
 const fetchLocation = async (domain) => {
   url = url.concat(`&domain=${domain}`);
 
   fetch(url)
     .then((response) => response.json())
-    .then((data) => setData(data));
+    .then((data) => setData(data))
+    .catch((error) => console.log(error));
 };
 
 const setData = (data) => {
   console.log(data);
+  
   addressInfo.innerText = data.ip;
   locationInfo.innerText = `${data.location.region}, ${data.location.country}`;
   timezoneInfo.innerText = `UTC${data.location.timezone}`;
   ispInfo.innerText = data.isp;
+
+  changeMapLocataion(data.location.lat, data.location.lng, data.isp);
+};
+
+const changeMapLocataion = (lat, lng, isp) => {
+  MAP.flyTo([lat, lng], 13, { animate: true, duration: 0.4 });
+
+  MARKER.setLatLng([lat, lng]);
+  MARKER.bindPopup(isp).openPopup();
 };
 
 form.addEventListener("submit", (event) => {
@@ -29,16 +60,4 @@ form.addEventListener("submit", (event) => {
   fetchLocation(enteredIp);
 });
 
-
-
-var map = L.map("map").setView([51.505, -0.09], 13);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
-
-L.marker([51.5, -0.09])
-  .addTo(map)
-  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-  .openPopup();
+initializeMap();
